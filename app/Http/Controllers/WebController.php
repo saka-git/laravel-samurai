b1308eac74cbac417751c4aedda4e21d73b4660a
“saka-git <sakamoto.tatsuya1997@gmail.com>
2023-02-27 21:50:50 +0900

    インポート機能実装

--- app/Http/Controllers/WebController.php
+++ app/Http/Controllers/WebController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Category;
 use App\Models\MajorCategory;
+use App\Models\Product;
 
 class WebController extends Controller
 {
@@ -13,7 +14,11 @@ class WebController extends Controller
         $categories = Category::all();
 
         $major_categories = MajorCategory::all();
+ 
+        $recently_products = Product::orderBy('created_at', 'desc')->take(4)->get();
+ 
+        $recommend_products = Product::where('recommend_flag', true)->take(3)->get();
 
-        return view('web.index', compact('major_categories', 'categories'));
+        return view('web.index', compact('major_categories', 'categories', 'recently_products', 'recommend_products'));
     }
 }
--- app/Models/Product.php
+++ app/Models/Product.php
@@ -10,6 +10,16 @@ use Kyslik\ColumnSortable\Sortable;
 class Product extends Model
 {
     use HasFactory, Favoriteable, Sortable;
+ 
+    protected $fillable = [
+        'name',
+        'description',
+        'price',
+        'category_id',
+        'image',
+        'recommend_flag',
+        'carriage_flag',
+    ];
 
     public function category()
     {
